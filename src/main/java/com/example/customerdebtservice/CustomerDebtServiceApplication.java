f8025ff8c45a3203c37f7fdc2a6ecacc41d1f4d6
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2022-03-01 13:55:38 +0200

    Initial commit

---
+++ src/main/java/com/example/customerdebtservice/CustomerDebtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerdebtservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerDebtServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerDebtServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/customerdebtservice/CustomerDebtServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customerdebtservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerDebtServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
