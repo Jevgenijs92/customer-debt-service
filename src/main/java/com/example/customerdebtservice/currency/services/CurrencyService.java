5d078a78d08600126161b2cdc023726b414c2d03
Jevgenijs Jaunslavietis <jevgenijs.jaunslavietis@gmail.com>
2022-03-02 14:24:58 +0200

    Added javadoc to currency service. Removed unused method.

--- src/main/java/com/example/customerdebtservice/currency/services/CurrencyService.java
+++ src/main/java/com/example/customerdebtservice/currency/services/CurrencyService.java
@@ -2,11 +2,17 @@ package com.example.customerdebtservice.currency.services;
 
 import com.example.customerdebtservice.currency.dto.CurrencyData;
 import com.example.customerdebtservice.currency.models.Currency;
+import com.example.customerdebtservice.shared.exceptions.ResourceExistsException;
 import com.example.customerdebtservice.shared.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CurrencyService {
+    /**
+     * Retrieve Currency object by code
+     * @param code contains 3 symbols
+     * @return Currency object
+     * @throws ResourceNotFoundException when Currency doesn't exist
+     */
     Currency getCurrencyByCode(String code) throws ResourceNotFoundException;
-    CurrencyData createCurrency(String name, String code, String symbol);
 }
--- src/main/java/com/example/customerdebtservice/currency/services/impl/CurrencyServiceImpl.java
+++ src/main/java/com/example/customerdebtservice/currency/services/impl/CurrencyServiceImpl.java
@@ -12,9 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CurrencyServiceImpl implements CurrencyService {
-
     private final CurrencyRepository currencyRepository;
-    private final CurrencyConverter currencyConverter;
 
     @Override
     public Currency getCurrencyByCode(String code) throws ResourceNotFoundException {
@@ -22,12 +20,4 @@ public class CurrencyServiceImpl implements CurrencyService {
                 .orElseThrow(()-> new ResourceNotFoundException("Cannot find currency for code: " + code));
     }
 
-    @Override
-    public CurrencyData createCurrency(String name, String code, String symbol) {
-        Currency currency = new Currency();
-        currency.setName(name);
-        currency.setCode(code);
-        currency.setSymbol(symbol);
-        return currencyConverter.convert(currencyRepository.save(currency));
-    }
 }
